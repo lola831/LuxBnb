bae2a933a02941576f2dc6e85ccc93dd05572494
lola <keepthechange2021@gmail.com>
2023-03-02 16:54:55 -0800

    Finished phase 1: API routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -16,5 +17,6 @@ const router = express.Router();
     });
  });
 
+router.use('/api', apiRouter);
 
 module.exports = router;
