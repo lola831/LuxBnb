e3b29824fe9e27fe208ac6d90a9007cb41a38d50
lola <keepthechange2021@gmail.com>
2023-03-04 11:48:38 -0800

    phase 3 complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,42 @@
 const router = require('express').Router();
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser); // all routes get passed through this global middleware first (utils/auth.js)
+
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+//   });
+
+//   // GET /api/set-token-cookie
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// //checks if user is logged in to view certain stuff?
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js'); //import this where you want to use this middleware
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
 
 module.exports = router;
