cb07c445ec502b02c0261b8b14b5c74c80207bcb
lola <keepthechange2021@gmail.com>
2023-03-04 12:49:46 -0800

    login complete and tested

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
-
-const { restoreUser } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser); // all routes get passed through this global middleware first (utils/auth.js)
 
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-//   });
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 //   // GET /api/set-token-cookie
 // const { setTokenCookie } = require('../../utils/auth.js');
