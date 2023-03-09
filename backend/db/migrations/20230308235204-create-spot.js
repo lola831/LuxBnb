3f37f7e406a5a9a1894bc845ab7b5dc1aad2343b
lola <keepthechange2021@gmail.com>
2023-03-08 17:20:42 -0800

    created spots table

---
+++ backend/db/migrations/20230308235204-create-spot.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ownerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'cascade'
+      },
+      address: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      city: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      state: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      country: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+      },
+      lat: {
+        type: Sequelize.INTEGER,
+        //allowNull: false,
+      },
+      lng: {
+        type: Sequelize.INTEGER,
+        //allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(49),
+        //allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING(500),
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Spots";
+    await queryInterface.dropTable(options);
+  }
+};
