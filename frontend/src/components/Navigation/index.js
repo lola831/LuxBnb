562ac6b01a06a387743f8851166e79757ebc2491
lola <keepthechange2021@gmail.com>
2023-05-07 13:17:29 -0700

    still working on styling nav

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -9,23 +9,31 @@ function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   return (
+    <div className='nav-container'>
     <ul>
+      <div className='airbnb'>
       <li>
         <NavLink exact to="/">airbnb</NavLink>
       </li>
+      </div>
       {isLoaded && (
         <>
           {sessionUser && (
+            <div className='create-spot'>
             <li>
               <NavLink exact to="/spots/new">Create a New Spot</NavLink>
             </li>
+            </div>
           )}
+          <div className='user-logo'>
           <li>
             <ProfileButton user={sessionUser} />
           </li>
+          </div>
         </>
       )}
     </ul>
+    </div>
   );
 }
 
